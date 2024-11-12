063344ef54155fd1bf87f4f020052d39cf42a5be
Luke Hiura <lhiura@sequel.ae>
2024-11-11 20:33:02 -0800

    numsteps

--- 2024/geico/number_of_steps_to_reduce_a_number.py
+++ 2024/geico/number_of_steps_to_reduce_a_number.py
@@ -43,4 +43,13 @@
 
 
 def numSteps(s: str) -> int:
-    pass
+    num = int(s, 2)
+    count = 0
+    while num != 1:
+        if num % 2 == 0:
+            num = num // 2
+            count += 1
+        else:
+            num += 1
+            count += 1
+    return count
